9090ca163cd94d1352f274fa5144bcd737545726
boccone <github@bocc.one>
2018-06-11 11:05:52 +0800

    bump version to 2.0

--- js/index.js
+++ js/index.js
@@ -18,162 +18,27 @@ layui.define(['layer', 'element', 'jquery'], function (exports) {
         ownerApprove: 0,
         user: "",
         daysInterest: 0,
-        blockDebitAddress: '',
+        blockDebitAddress: ''
     }
-
-    if (typeof web3 == 'undefined') {
-        layer.msg('您需要先安装 MetaMask', { shade: 0.3, time: 30000 });
-    }
-
-    if (web3.eth.coinbase === null) {
-        layer.msg('请解锁 MetaMask 并刷新此页面', { shade: 0.3, time: 30000 });
-    }
-
-    web3.version.getNetwork(function (e, networkID) {
-        TetherTokenAddress = TetherTokenJSON.networks[networkID].address;
-        BlockDebitAddress = BlockDebitJSON.networks[networkID].address;
-        cb.user = web3.eth.coinbase;
-
-        eth = new Eth(web3.currentProvider);
-        TetherTokenABI = TetherTokenJSON.abi
-        TetherToken = eth.contract(TetherTokenABI).at(TetherTokenAddress);
-
-        BlockDebitABI = BlockDebitJSON.abi
-        BlockDebit = eth.contract(BlockDebitABI).at(BlockDebitAddress);
-
-        update()
-        updateDaysInterest()
-
-        $('#contractadres').text(BlockDebitAddress);
-        $('#wallet-address').text(cb.user);
-        $('#have-dth').text(cb.currentETH / 10 ** 18);
-        $('#have-usdt').text(cb.currentUSDT / 10 ** 6);
-        $('#mortgage-eth').text(cb.debitInfo[0].toString() / 10 ** 18);
-        $('#repay-usdt').text(cb.debitInfo[1].toString() / 10 ** 6);
-        $('#repay-day').text(new Date(cb.debitInfo[2].toNumber() * 1000).format("yyyy-MM-dd hh:mm:ss"));
-        $('#loan-rate').text(cb.exchangeRate / 10 ** 4);
-
-        window.TetherTokenAddress = TetherTokenAddress
-        window.BlockDebitAddress = BlockDebitAddress
-        window.TetherToken = TetherToken
-        window.BlockDebit = BlockDebit
-    });
-
-    function giveDebit(giveDebitEther) {
-        console.log(days, giveDebitEther)
-        BlockDebit.giveDebit(days, {
-            'from': web3.eth.coinbase,
-            'value': web3.toWei(giveDebitEther, 'ether'),
-            'gasLimit': 400000,
-        }).then(function () {
-            update()
-        })
-    }
-
-    function backDebit1() {
-        TetherToken.approve(BlockDebitAddress, 0, {
-            'from': web3.eth.coinbase
-        })
-    }
-
-    function backDebit2() {
-        console.log('backDebit2', BlockDebitAddress, cb.debitInfo[1])
-        TetherToken.approve(BlockDebitAddress, cb.debitInfo[1], {
-            'from': web3.eth.coinbase
-        });
-    }
-
-    function backDebit3() {
-        console.log('backDebit3')
-        BlockDebit.backDebit({
-            'from': web3.eth.coinbase
-        })
-    }
-
-
-    function update() {
-        BlockDebit.owner().then(r => cb.owner = r[0]).then(function () {
-            TetherToken.balanceOf(cb.owner).then(
-                r => cb.ownerToken = r[0].toNumber()
-            ).then(function () {
-                $('#ownertoken').text(cb.ownerToken / 10 ** 6)
-            });
-        }).then(function () {
-            TetherToken.allowance(cb.owner, BlockDebitAddress).then(
-                r => cb.ownerApprove = r[0].toNumber()
-            ).then(function () {
-                $('#ownerapprove').text(cb.ownerApprove / 10 ** 6)
-            })
-        });
-
-        // ------------------------------------------------------------
-        cb.user = web3.eth.coinbase
-
-        // ------------------------------------------------------------
-        web3.eth.getBalance(web3.eth.coinbase, function (err, balance) {
-            if (err === null) {
-                cb.currentETH = balance.toNumber()
-                $('#have-dth').text(cb.currentETH / 10 ** 18);
-            }
-        });
-        TetherToken.balanceOf(web3.eth.coinbase).then(
-            r => cb.currentUSDT = r[0].toNumber()
-        ).then(
-            r => $('#have-usdt').text(cb.currentUSDT / 10 ** 6)
-        );
-
-        // 借贷信息
-        BlockDebit.info(web3.eth.coinbase).then(
-            r => cb.debitInfo = r[0]
-        ).then(function () {
-            if (cb.debitInfo[0].toString() === '0') {
-                console.log('hide')
-                $('#debit-info').hide()
-                $('#debit-info-none').show()
-            } else {
-                console.log('show')
-                $('#mortgage-eth').text(cb.debitInfo[0].toString() / 10 ** 18);
-                $('#repay-usdt').text(cb.debitInfo[1].toString() / 10 ** 6);
-                $('#repay-day').text(new Date(cb.debitInfo[2].toNumber() * 1000).format("yyyy-MM-dd hh:mm:ss"));
-                $('#debit-info').show()
-                $('#debit-info-none').hide()
-            }
-        })
-        // 当前借贷利率
-        BlockDebit.exchangeRate().then(
-            r => cb.exchangeRate = r[0].toNumber()
-        ).then(function () {
-            $('#loan-rate').text(cb.exchangeRate / 10 ** 4);
-        })
-    }
-
-    function updateDaysInterest() {
-        BlockDebit.getInterest(cb.giveDebitDay).then(function (r) {
-            web3.eth.getBlockNumber(function (err, n) {
-                if (n > r[0][1].toNumber()) {
-                    cb.daysInterest = r[0][2].toNumber()
-                } else {
-                    cb.daysInterest = r[0][0].toNumber()
-                }
-            })
-        })
-    }
-
-    window.giveDebit = giveDebit
-    window.backDebit1 = backDebit1
-    window.backDebit2 = backDebit2
-    window.backDebit3 = backDebit3
-    window.update = update
-    window.cb = cb
 
     $('#apply-for-loan').click(function () {
         console.log("跳转" + $("#bocc-main").offset().top);
         $("html,body").animate({ scrollTop: $("#bocc-main").offset().top }, 500);
     });
 
-    $('.bocc-content-item').on('click', function () {
-        console.log($(this).attr("data-days"));
-        days = $(this).attr("data-days");
+    $('.bocc-loan-btn').hover(function () {
+        $(this).text('立即借款');
+    }, function () {
+        $(this).text($(this).attr('text-data'));
+    });
+
+    $('.bocc-loan-btn').on('click', function () {
+        if (typeof web3 == 'undefined') {
+            layer.msg('您需要先安装 MetaMask');
+            return;
+        }
+        console.log($(this).attr("days-data"));
+        days = $(this).attr("days-data");
         layer.open({
             type: 2,
             title: false,
@@ -209,7 +74,170 @@ layui.define(['layer', 'element', 'jquery'], function (exports) {
                 // }
             }
         });
-    })
+    });
+
+    if (typeof web3 == 'undefined') {
+        //layer.msg('您需要先安装 MetaMask', { shade: 0.3, time: 30000 });
+        console.log('安装metamask');
+        $('#bocc-user-info').hide();
+        $('#bocc-no-metamask').show();
+        return;
+    } else {
+        console.log('隐藏');
+        $('#bocc-user-info').show();
+        $('#bocc-no-metamask').hide();
+    }
+
+    if (web3.eth.coinbase === null) {
+        layer.msg('请解锁 MetaMask 并刷新此页面');
+        return;
+    }
+
+    web3.version.getNetwork(function (e, networkID) {
+        TetherTokenAddress = TetherTokenJSON.networks[networkID].address;
+        BlockDebitAddress = BlockDebitJSON.networks[networkID].address;
+        cb.user = web3.eth.coinbase;
+
+        eth = new Eth(web3.currentProvider);
+        TetherTokenABI = TetherTokenJSON.abi
+        TetherToken = eth.contract(TetherTokenABI).at(TetherTokenAddress);
+
+        BlockDebitABI = BlockDebitJSON.abi
+        BlockDebit = eth.contract(BlockDebitABI).at(BlockDebitAddress);
+
+        update()
+        updateDaysInterest()
+
+        $('#contractadres').text(BlockDebitAddress);
+        $('#wallet-address').text(cb.user);
+        $('#have-eth').text(cb.currentETH / 10 ** 18);
+        $('#have-usdt').text(cb.currentUSDT / 10 ** 6);
+        $('#mortgage-eth').text(cb.debitInfo[0].toString() / 10 ** 18);
+        $('#repay-usdt').text(cb.debitInfo[1].toString() / 10 ** 6);
+        $('#repay-date').text(new Date(cb.debitInfo[2].toNumber() * 1000).format("yyyy-MM-dd hh:mm:ss"));
+        $('#loan-rate').text(cb.exchangeRate / 10 ** 4);
+    });
+
+    function giveDebit(giveDebitEther) {
+        console.log(days, giveDebitEther)
+        BlockDebit.giveDebit(days, {
+            'from': web3.eth.coinbase,
+            'value': web3.toWei(giveDebitEther, 'ether'),
+            'gasLimit': 400000,
+        }).then(function () {
+            update()
+        })
+    }
+
+    function backDebit1() {
+        TetherToken.approve(BlockDebitAddress, 0, {
+            'from': web3.eth.coinbase
+        })
+    }
+
+    function backDebit2() {
+        console.log('backDebit2', BlockDebitAddress, cb.debitInfo[1])
+        TetherToken.approve(BlockDebitAddress, cb.debitInfo[1], {
+            'from': web3.eth.coinbase
+        });
+    }
+
+    function backDebit3() {
+        console.log('backDebit3')
+        BlockDebit.backDebit({
+            'from': web3.eth.coinbase
+        })
+    }
+
+    function backDebit() {
+        console.log('backDebit2', BlockDebitAddress, cb.debitInfo[1])
+        TetherToken.approve(BlockDebitAddress, cb.debitInfo[1], {
+            'from': web3.eth.coinbase
+        }).then(function () {
+            console.log('backDebit3')
+            BlockDebit.backDebit({
+                'from': web3.eth.coinbase
+            })
+        })
+    }
+
+    function update() {
+        BlockDebit.owner().then(r => cb.owner = r[0]).then(function () {
+            TetherToken.balanceOf(cb.owner).then(
+                r => cb.ownerToken = r[0].toNumber()
+            ).then(function () {
+                $('#ownertoken').text(cb.ownerToken / 10 ** 6)
+            });
+        }).then(function () {
+            TetherToken.allowance(cb.owner, BlockDebitAddress).then(
+                r => cb.ownerApprove = r[0].toNumber()
+            ).then(function () {
+                $('#ownerapprove').text(cb.ownerApprove / 10 ** 6)
+            })
+        });
+
+        // ------------------------------------------------------------
+        cb.user = web3.eth.coinbase
+
+        // ------------------------------------------------------------
+        web3.eth.getBalance(web3.eth.coinbase, function (err, balance) {
+            if (err === null) {
+                cb.currentETH = balance.toNumber()
+                a = new Number(cb.currentETH / 10 ** 18)
+                $('#have-eth').text(a.toFixed(3));
+            }
+        });
+        TetherToken.balanceOf(web3.eth.coinbase).then(function (r) {
+            cb.currentUSDT = r[0].toNumber()
+            a = new Number(cb.currentUSDT / 10 ** 6)
+            $('#have-usdt').text(a.toFixed(3))
+        });
+
+        // 借贷信息
+        BlockDebit.info(web3.eth.coinbase).then(
+            r => cb.debitInfo = r[0]
+        ).then(function () {
+            if (cb.debitInfo[0].toString() === '0') {
+                console.log('hide')
+                $('#debit-info').hide()
+                $('#debit-info-back').hide()
+                $('#debit-info-none').show()
+            } else {
+                console.log('show')
+                $('#mortgage-eth').text(cb.debitInfo[0].toString() / 10 ** 18);
+                $('#repay-usdt').text(cb.debitInfo[1].toString() / 10 ** 6);
+                $('#repay-date').text(new Date(cb.debitInfo[2].toNumber() * 1000).format("yyyy-MM-dd"));
+                $('#debit-info').show()
+                $('#debit-info-none').hide()
+            }
+        })
+        // 当前借贷利率
+        BlockDebit.exchangeRate().then(
+            r => cb.exchangeRate = r[0].toNumber()
+        ).then(function () {
+            $('#loan-rate').text(cb.exchangeRate / 10 ** 4);
+        })
+    }
+
+    function updateDaysInterest() {
+        BlockDebit.getInterest(cb.giveDebitDay).then(function (r) {
+            web3.eth.getBlockNumber(function (err, n) {
+                if (n > r[0][1].toNumber()) {
+                    cb.daysInterest = r[0][2].toNumber()
+                } else {
+                    cb.daysInterest = r[0][0].toNumber()
+                }
+            })
+        })
+    }
+
+    window.giveDebit = giveDebit;
+    window.backDebit1 = backDebit1;
+    window.backDebit2 = backDebit2;
+    window.backDebit3 = backDebit3;
+    window.backDebit = backDebit;
+    window.update = update;
+    window.cb = cb;
 
     Date.prototype.format = function (fmt) {
         var o = {
